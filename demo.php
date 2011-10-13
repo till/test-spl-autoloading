9b4cd1662102fe714f6c2236d4586af70de93494
till <till@php.net>
2011-10-13 15:51:07 +0200

    small cleanups to make it more obvious

--- demo.php
+++ demo.php
@@ -1,28 +1,28 @@
 #!/usr/bin/env php
 <?php
 /**
- * @desc Zend Framework without require_once
- */
-$zfPath = '/Users/till/Documents/workspaces/imagineeasy_v5/trunk/library';
-
-/**
  * Loads classname-filename-convention.
  */
 function test1($class) {
+    echo ">> test1 called" . PHP_EOL;
     require str_replace('_', '/', $class) . '.php';
 }
 /**
  * Loads some vendor specific stuff.
  */
 function test2($class) {
+    echo ">> test2 called" . PHP_EOL;
     require __DIR__ . '/vendor.php';
 }
 
+/**
+ * @desc register both in a chain
+ */
 spl_autoload_register('test1');
 spl_autoload_register('test2');
 
 $foobar = new Foobar();
-echo "Foobar loaded." . PHP_EOL;
+echo "Foobar loaded." . PHP_EOL . PHP_EOL;
 
 /**
  * @desc This class is never loaded.
